8e4afd0f1fd86b98f9fd0d5435a8da127f1d681a
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-16 16:50:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,7 @@ names_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   badge_name =  "Hello, my name is #{name}."
+  
 end
 
 def batch_badge_creator(name_array)
