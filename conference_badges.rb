ad54035b467f76f3d172b7f69c1ed03d1c8f53aa
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-16 18:37:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,26 +10,21 @@ def room_assigner(value, index)
 end
 
 def batch_badge_creator(names)
-name_message_array = []
-names.each do |name|
-name_and_message = badge_maker(name)
-name_message_array.push(name_and_message)
+  name_message_array = []
+  names.each do |name|
+  name_and_message = badge_maker(name)
+  name_message_array.push(name_and_message)
   end
-name_message_array
+  name_message_array
 end
 
 def assign_rooms(attendees)
-attendees_rooms_array = []
-attendees.each.with_index(1) do |value, index|
-attendee_and_room = room_assigner(value, index)
-attendees_rooms_array.push(attendee_and_room)
+  attendees_rooms_array = []
+  attendees.each.with_index(1) do |value, index|
+  attendee_and_room = room_assigner(value, index)
+  attendees_rooms_array.push(attendee_and_room)
   end
-attendees_rooms_array
+  attendees_rooms_array
 end
 
-#def assign_rooms(attendees)
-#  name_and_room = []
-#attendees.each.with_index(1) do |value, index|
-#puts "Hello, #{value}! You'll be assigned to room #{index}!"
- # end
-#end
+
