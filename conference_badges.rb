089bbdfe73b618da8ccc04ebf3bec1a77e8f64a1
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-16 17:02:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,6 @@
 # Write your code here.
 names_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+messages = []
 def badge_maker(name)
   badge_name =  "Hello, my name is #{name}."
   
@@ -7,7 +8,7 @@ end
 
 def batch_badge_creator(attendees)
 attendees.each do |name|
-badge_messages.push(badge_maker(name))
+messages.push(badge_maker(name))
   end
 end
 
