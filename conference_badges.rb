72ffb2e529e08938080d9947a9aa7ebc33fd37c8
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-16 17:55:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ name_message_array
 end
 
 def assign_rooms(attendees)
-attendees.each_with_index { | attendee, index| puts "Hello, #{attendee}! You'll be assigned to room #{index}!"}
+attendees.each_with_index { |attendee, index| puts "Hello, #{attendee}! You'll be assigned to room #{index}!"}
   end
   #"Hello, _____! You'll be assigned to room _____!"
 end
