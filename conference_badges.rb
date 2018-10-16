7541ecf42afac502c332684e915fe461fff08e4c
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-16 19:26:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def batch_badge_creator(names)
   name_and_message = badge_maker(name)
   name_message_array.push(name_and_message)
   end
-  printer(name_message_array)
+  #printer(name_message_array)
   name_message_array
 end
 
@@ -25,7 +25,7 @@ def assign_rooms(attendees)
   attendee_and_room = room_assigner(value, index)
   attendees_rooms_array.push(attendee_and_room)
   end
-  printer(attendees_rooms_array)
+  #printer(attendees_rooms_array)
   attendees_rooms_array
 
 end
